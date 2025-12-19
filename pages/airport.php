b82dc7d651f7e1077f103bf4baf9ccc72cf4e35c
Alex Witherspoon <Alex@AlexWitherspoon.com>
2025-12-18 20:27:19 -0800

    Make preconnect links conditional based on weather source
    Only preconnect to APIs that are actually used by each airport's
    weather source configuration. This prevents unnecessary DNS lookups
    and TCP connections for unused data sources, improving page load
    performance.
    
    Previously, all weather source APIs were preconnected regardless
    of which sources were actually configured for the airport. Now only
    the primary weather source API and METAR (if enabled) are preconnected.

--- pages/airport.php
+++ pages/airport.php
@@ -135,15 +135,36 @@ if (isset($airport['webcams']) && count($airport['webcams']) > 0) {
     ?>
     <link rel="preconnect" href="<?= $protocol ?>://<?= htmlspecialchars($host) ?>">
     
-    <!-- Resource hints for external APIs -->
-    <link rel="preconnect" href="https://swd.weatherflow.com" crossorigin>
-    <link rel="preconnect" href="https://api.ambientweather.net" crossorigin>
-    <link rel="preconnect" href="https://api.weatherlink.com" crossorigin>
-    <link rel="preconnect" href="https://aviationweather.gov" crossorigin>
-    <link rel="dns-prefetch" href="https://swd.weatherflow.com">
-    <link rel="dns-prefetch" href="https://api.ambientweather.net">
-    <link rel="dns-prefetch" href="https://api.weatherlink.com">
-    <link rel="dns-prefetch" href="https://aviationweather.gov">
+    <!-- Resource hints for external APIs (conditional based on weather source) -->
+    <?php
+    // Only preconnect to APIs that are actually used by this airport's weather source
+    $weatherSourceType = isset($airport['weather_source']['type']) ? $airport['weather_source']['type'] : null;
+    $needsMetar = isMetarEnabled($airport);
+    
+    switch ($weatherSourceType) {
+        case 'tempest':
+            echo "    <link rel=\"preconnect\" href=\"https://swd.weatherflow.com\" crossorigin>\n";
+            echo "    <link rel=\"dns-prefetch\" href=\"https://swd.weatherflow.com\">\n";
+            break;
+        case 'ambient':
+            echo "    <link rel=\"preconnect\" href=\"https://api.ambientweather.net\" crossorigin>\n";
+            echo "    <link rel=\"dns-prefetch\" href=\"https://api.ambientweather.net\">\n";
+            break;
+        case 'weatherlink':
+            echo "    <link rel=\"preconnect\" href=\"https://api.weatherlink.com\" crossorigin>\n";
+            echo "    <link rel=\"dns-prefetch\" href=\"https://api.weatherlink.com\">\n";
+            break;
+        case 'pwsweather':
+            echo "    <link rel=\"preconnect\" href=\"https://api.aerisapi.com\" crossorigin>\n";
+            echo "    <link rel=\"dns-prefetch\" href=\"https://api.aerisapi.com\">\n";
+            break;
+    }
+    
+    if ($needsMetar) {
+        echo "    <link rel=\"preconnect\" href=\"https://aviationweather.gov\" crossorigin>\n";
+        echo "    <link rel=\"dns-prefetch\" href=\"https://aviationweather.gov\">\n";
+    }
+    ?>
     
     <?php
     // Enhanced meta tags
